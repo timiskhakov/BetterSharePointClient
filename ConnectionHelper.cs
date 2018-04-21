c751786408ce3c0269753fb60a3d781582f11f52
Tim <timiskhakov@gmail.com>
2018-04-21 11:01:04 +0300

    Add connection helper

---
+++ ConnectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BetterSharePointClient
+{
+    internal static class ConnectionHelper
+    {
+        internal static X509Certificate2 GetCertificate(string serialNumber)
+        {
+            var store = new X509Store(StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+            var collection = store.Certificates.Find(X509FindType.FindBySerialNumber, serialNumber, false);
+            store.Close();
+            return collection.Count > 0
+                ? collection[0]
+                : null;
+        }
+    }
+}
